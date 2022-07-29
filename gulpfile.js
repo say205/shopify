d2a21fb5629aa47281d01adfb3257559e25eb0f3
ductrung7747 <ductrung.7747@gmail.com>
2022-07-29 08:39:50 +0700

    ⚡ Renew Code

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function() {
-    return gulp.src('assets/*.scss')
+    return gulp.src('styles/*.scss')
             .pipe(sass())
             .pipe(gulp.dest('assets'))
 });
 
 gulp.task('watch', function() {
-    gulp.watch('assets/**/*.scss', gulp.series('sass'));
+    gulp.watch('styles/**/*.scss', gulp.series('sass'));
 });
